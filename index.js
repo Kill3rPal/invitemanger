3acb48ddbc87c8d7b616840250cfa525f53b661b
Kill3rPal <68537603+Kill3rPal@users.noreply.github.com>
2020-10-10 13:51:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -341,6 +341,40 @@ client.on('message', async message => {
     }
   });
 
+const developers = ["720115877873451048"] // ايدي صاحب البوت
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+ 
+  if (message.content.startsWith(prefix + 'p')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (prefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(prefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(prefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(prefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
 
 
-  client.login(token)
+
+  client.login('NzY0NDM5NjA3Mjg5ODM5NjE2.X4GR3A.1jbXrj0kfNss_rb8fDTlXd9ACXA')
